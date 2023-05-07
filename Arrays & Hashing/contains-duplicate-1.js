17ee43cd91dd3fbd4e47dc1346ada1fb9299fadf
anurag-majumdar <anurag.majumdar92@gmail.com>
2023-05-07 14:17:41 +0530

    refactor: contains-duplicate-1 js

--- Arrays & Hashing/contains-duplicate-1.js
+++ Arrays & Hashing/contains-duplicate-1.js
@@ -4,12 +4,11 @@
  */
 const containsDuplicate = (nums) => {
     const frequencyMap = new Map();
-    const numsLength = nums.length;
-    for (let index = 0; index < numsLength; index++) {
-        if (frequencyMap.has(nums[index])) {
+    for (let num of nums) {
+        if (frequencyMap.has(num)) {
             return true;
         } else {
-            frequencyMap.set(nums[index], 1);
+            frequencyMap.set(num, 1);
         }
     }
     return false;
