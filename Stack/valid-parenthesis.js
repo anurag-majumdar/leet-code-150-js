b26bab541ab55c837011a55689a0e6f4fc63e4ec
anurag-majumdar <anurag.majumdar92@gmail.com>
2023-05-07 15:14:36 +0530

    feat: Add valid-parenthesis js

---
+++ Stack/valid-parenthesis.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = (s) => {
+    if (s[0] === ')' || s[0] === ']' || s[0] === '}') return false;
+
+    const stack = [];
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (bracketMap[char]) {
+            if (stack.length > 0 && stack[0] === bracketMap[char]) {
+                stack.shift();
+            } else {
+                return false;
+            }
+        } else {
+            stack.unshift(char);
+        }
+    }
+
+    if (stack.length === 0) return true;
+    else return false;
+};
