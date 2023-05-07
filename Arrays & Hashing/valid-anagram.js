34d243204da2f2263834d4ff9ecfd7fac9153452
anurag-majumdar <anurag.majumdar92@gmail.com>
2023-05-07 14:26:14 +0530

    feat: Add valid-anagram js

---
+++ Arrays & Hashing/valid-anagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = (s, t) => {
+    if (s.length != t.length) return false;
+    const frequencyMap = new Map();
+    for (let char of s) {
+        if (frequencyMap.has(char)) {
+            frequencyMap.set(char, frequencyMap.get(char) + 1);
+        } else {
+            frequencyMap.set(char, 1);
+        }
+    }
+
+    for (let char of t) {
+        if (!frequencyMap.has(char)) return false;
+        else {
+            const charFrequency = frequencyMap.get(char);
+            if (charFrequency === 1) frequencyMap.delete(char);
+            else frequencyMap.set(char, charFrequency - 1);
+        }
+    }
+    return true;
+};
