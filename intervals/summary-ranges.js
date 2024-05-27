2b68dfb8f6c3bc838adc4556a304cb006b3ce4ca
Anurag Majumdar <anurag.majumdar92@gmail.com>
2024-05-28 01:57:20 +0530

    feat: Add summary ranges solution

---
+++ intervals/summary-ranges.js
@@ -0,0 +1,34 @@
+/**
+ * O(n) time | O(n) space
+ * 
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+const summaryRanges = function (nums) {
+    const ranges = [];
+    let startIndex = 0, currentIndex = 0, n = nums.length, expectedNum = nums[0];
+
+    while (currentIndex < n) {
+        if (expectedNum + 1 === nums[currentIndex + 1]) {
+            currentIndex++;
+            expectedNum++;
+        } else {
+            ranges.push(encodeRange([nums[startIndex], nums[currentIndex]]));
+            expectedNum = nums[currentIndex + 1];
+            currentIndex++;
+            startIndex = currentIndex;
+        }
+    }
+    return ranges;
+};
+
+const encodeRange = ([startVal, endVal]) => {
+    if (startVal !== endVal) {
+        return `${startVal}->${endVal}`;
+    } else {
+        return `${startVal}`;
+    }
+};
+
+console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
+console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]));
