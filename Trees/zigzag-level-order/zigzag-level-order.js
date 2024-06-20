9004ccd62919e01ca238f4bd487e042cc7b22368
Anurag Majumdar <anurag.majumdar92@gmail.com>
2024-06-20 10:45:15 +0530

    Update zigzag level order traversal

--- Trees/zigzag-level-order/zigzag-level-order.js
+++ Trees/zigzag-level-order/zigzag-level-order.js
@@ -39,3 +39,57 @@ var zigzagLevelOrder = function (root) {
     }
     return result;
 };
+
+// Approach 2
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder2 = function (root) {
+    if (!root) {
+        return [];
+    }
+
+    const result = [];
+    const queue = [root];
+    let level = 1;
+    let rtl = false;
+    while (queue.length > 0) {
+        let n = queue.length;
+        const levelNodes = [];
+        while (n > 0) {
+            if (rtl) {
+                const node = queue.pop();
+                levelNodes.push(node.val);
+                if (node.right) {
+                    queue.unshift(node.right);
+                }
+                if (node.left) {
+                    queue.unshift(node.left);
+                }
+            } else {
+                const node = queue.shift();
+                levelNodes.push(node.val);
+                if (node.left) {
+                    queue.push(node.left);
+                }
+                if (node.right) {
+                    queue.push(node.right);
+                }
+            }
+            n--;
+        }
+        rtl = !rtl;
+        result.push(levelNodes);
+        level++;
+    }
+    return result;
+};
