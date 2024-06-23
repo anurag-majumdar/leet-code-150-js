d6896fbf8716f4d9731551eb86c0d4cf3cd7fff2
Anurag Majumdar <anurag.majumdar92@gmail.com>
2024-06-23 14:25:14 +0530

    feat: Add buy and sell stocks ii

---
+++ array-string/buy-sell-stock-ii.js
@@ -0,0 +1,29 @@
+/**
+ * O(n) solution.
+ * 
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfitOptimal = function (prices) {
+    if (prices.length <= 1) {
+        return 0;
+    }
+    let totalProfit = 0, maxSellProfit = 0;
+    let left = 0, right = 1;
+    const n = prices.length;
+    while (right < n) {
+        if (prices[left] <= prices[right]) {
+            maxSellProfit = Math.max(maxSellProfit, prices[right] - prices[left]);
+        } else if (prices[left] > prices[right]) {
+            left = right;
+        }
+        if (prices[right] > prices[right + 1]) {
+            atleastOneDip = true;
+            totalProfit += maxSellProfit;
+            maxSellProfit = 0;
+            left = right + 1;
+        }
+        right++;
+    }
+    return totalProfit + maxSellProfit;
+};
