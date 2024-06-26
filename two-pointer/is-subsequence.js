021068feb0849ad58d56019389be988ee8f0173b
Anurag Majumdar <anurag.majumdar92@gmail.com>
2024-06-26 17:27:35 +0530

    refactor: Update is subsequence

--- two-pointer/is-subsequence.js
+++ two-pointer/is-subsequence.js
@@ -8,10 +8,8 @@ const isSubsequence = function (s, t) {
     while (i < s.length && j < t.length) {
         if (s[i] === t[j]) {
             i++;
-            j++;
-        } else {
-            j++;
         }
+        j++;
     }
     if (i === s.length) {
         return true;
