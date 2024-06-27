a82ab2d25c8a0cb51abe01c4dea86f6bd8a7c8f2
Anurag Majumdar <anurag.majumdar92@gmail.com>
2024-06-27 16:14:01 +0530

    feat: Add remove element solution

---
+++ array-string/remove-element.js
@@ -0,0 +1,50 @@
+/**
+ * Ideal solution.
+ * O(n) time complexity, 2 pointer approach.
+ * 
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElementIdeal = function (nums, val) {
+    let left = 0, right = nums.length - 1;
+    while (left < right) {
+        if (nums[left] === val && nums[right] !== val) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+        if (nums[left] !== val) {
+            left++;
+        }
+        if (nums[right] === val) {
+            right--;
+        }
+    }
+    if (left === right && nums[left] === val) {
+        return left;
+    }
+    return right + 1;
+};
+
+function swap(nums, i, j) {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+};
+
+/**
+ * O(n) time complexity.
+ * O(n) space complexity.
+ * 
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElementSubOptimal = function (nums, val) {
+    const temp = nums.filter(num => num !== val);
+    for (let i = 0; i < temp.length; i++) {
+        nums[i] = temp[i];
+    }
+    return temp.length;
+};
