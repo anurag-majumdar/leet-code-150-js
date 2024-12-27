84999e6e4b1d5c2bbcc9b5a486a0f70ce0523970
Anurag Majumdar <anurag.majumdar92@gmail.com>
2024-12-27 17:03:27 +0530

    refactor: Refactor arrays & strings

--- src/array-string/__tests__/contains-duplicate-1.test.js
+++
@@ -1,9 +0,0 @@
-const containsDuplicate = require('../contains-duplicate-1');
-
-describe('contains-duplicate-1', () => {
-  it('Test for duplicate in [7, 5, 5, 23, 1]', () => {
-    const input = [7, 5, 5, 23, 1];
-    const result = containsDuplicate(input);
-    expect(result).toBe(true);
-  });
-});
---
+++ src/strings/word-search.js
@@ -0,0 +1,10 @@
+/**
+ * LC 79: Word Search
+ *
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+const exist = (board, word) => {
+
+};
