f0fcf53a279b2daa73e95c897fba50ef914ed547
Anurag Majumdar <anurag.majumdar92@gmail.com>
2024-12-27 18:25:30 +0530

    feat: Add word search problem & solution

---
+++ src/strings/__tests__/word-search.test.js
@@ -0,0 +1,16 @@
+const exist = require('../word-search');
+
+describe('LC 79: Word Search', () => {
+  it('Case 1:', () => {
+    const board = [
+      ['A', 'B', 'C', 'E'],
+      ['S', 'F', 'C', 'S'],
+      ['A', 'D', 'E', 'E'],
+    ];
+    const word = 'ABCCED';
+    const expectedOutput = true;
+
+    const actualOutput = exist(board, word);
+    expect(actualOutput).toBe(expectedOutput);
+  });
+});
--- src/strings/word-search.js
+++ src/strings/word-search.js
@@ -1,3 +1,29 @@
+const searchWord = (pos, wordIndex) => {
+    const [i, j] = pos;
+
+    if (wordIndex === W) {
+      return true;
+    }
+
+    if (board[i][j] !== word[wordIndex]) {
+      return false;
+    }
+
+    tempChar = board[i][j];
+    board[i][j] = '#';
+
+    const found =
+      searchWord(i + 1, j, wordIndex + 1) ||
+      searchWord(i - 1, j, wordIndex + 1) ||
+      searchWord(i, j + 1, wordIndex + 1) ||
+      searchWord(i, j - 1, wordIndex + 1);
+
+    board[i][j] = tempChar;
+    return found;
+  };
+
+
+
 /**
  * LC 79: Word Search
  *
@@ -6,5 +32,25 @@
  * @return {boolean}
  */
 const exist = (board, word) => {
+  const m = board.length;
+  const n = board[0].length;
+  const W = word.length;
 
+  if (m === 1 && n === 1) {
+    return board[0][0] === word;
+  }
+
+  
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (searchWord([i, j], 0)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
+
+module.exports = exist;
